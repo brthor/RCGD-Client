75e27148419333de840b1d630af0d91add35cfea
Mozoby <brthornbury@csupomona.edu>
2015-04-05 03:08:09 -0700

    client final v1

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ connect().use(serveStatic('www')).listen(8080);
 
 //Connect to the Control Server
 var net = require('net')
-var auth = require('rcgdauth')
+//var auth = require('rcgdauth')
 
 var serverHost = '0.0.0.0'
 var serverPort = 6969
 
 var client = new net.Socket();
 client.connect(serverPort, serverHost,function(){
-	auth.authorize(client);
+	//auth.authorize(client);
 });
 
 // No Data should be sent. Any data is an error, just close.
@@ -35,28 +35,47 @@ client.on('close', function() {
 var XboxController = require('xbox-controller');
 var xbox = new XboxController;
 
+function writeData(data){
+	data['clientType'] = 'c'
+	client.write(data)
+}
+
 xbox.on('a:press', function (key) {
-  //console.log(key + ' press');
+	console.log('a', 0);
+  	writeData({'id': 'A', 'value':1});
+});
+
+xbox.on('a:release', function (key) {
+	console.log('a', 0);
+  	writeData({'id': 'A', 'value':0});
+});
+
+xbox.on('b:press', function (key) {
+  console.log('b', 0);
+  writeData({'id': 'B', 'value':1});
 });
 
 xbox.on('b:release', function (key) {
-  //console.log(key+' release');
+  console.log('b', 0);
+  writeData({'id': 'B', 'value':0});
 });
 
 xbox.on('lefttrigger', function(position){
-  //console.log('lefttrigger', position);
+  console.log('lefttrigger', position);
+  writeData({'id': 'LEFT_TRIGGER', 'value':position});
 });
 
 xbox.on('righttrigger', function(position){
   console.log('righttrigger', position);
-  client.write({'id': 'throttle', 'value':position});
+  writeData({'id': 'RIGHT_TRIGGER', 'value':position});
 });
 
 xbox.on('left:move', function(position){
   console.log('left:move', position);
-  client.write({'id': 'turn', 'value':position});
+  writeData({'id': 'LEFT_STICK', 'value':position});
 });
 
 xbox.on('right:move', function(position){
-  //console.log('right:move', position);
-});
+	console.log('right:move', position);
+  	client.write({'id': 'RIGHT_STICK', 'value':position});
+ });
